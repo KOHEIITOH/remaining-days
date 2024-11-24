b506873a219042315a57b1f8604d3a4e4ef5c05e
KOHEIITOH <koheiito39@gmail.com>
2024-11-24 13:35:51 +0900

    今年度の残日数画面の追加

--- fiscal_year.js
+++ fiscal_year.js
@@ -12,17 +12,17 @@ window.addEventListener('resize', () => {
 let today = new Date();
 today.setHours(0,0,0,0);
 
-// 今年の残日数の算出
+// 今年度の残日数の算出
 const year = today.getFullYear(),
       month = ("0" + (today.getMonth() + 1)).slice(-2), // JavaScriptでは月は0から始まるため1を足す
       date = ("0" + today.getDate()).slice(-2), // 日にちが一桁のときに二桁表示にするため
       week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"],
       day_of_week = week[today.getDay()],
       format_date = year + "." + month + "." + date + " " + day_of_week,
-      end_year = new Date(today.getFullYear(), 11, 31),// JavaScriptでは年は0から始まるため11は12月を表す
-      ms_per_day = 24 * 60 * 60 * 1000,
-      float_remaining_days = (end_year.getTime() - today.getTime()) / ms_per_day,
-      format_calendar_remaining_days = Math.ceil(float_remaining_days);
+      fiscal_year_end = today.getMonth() >= 3 ? today.getFullYear() + 1 : today.getFullYear(),
+      end_of_fiscal_year = new Date(fiscal_year_end, 2, 31),
+      fiscal_diff = end_of_fiscal_year.getTime() - today.getTime(),
+      format_fiscal_remaining_days = Math.ceil(fiscal_diff / (24 * 60 * 60 * 1000));
 
 document.getElementById("today").innerHTML = format_date;
-document.getElementById("cy-days").innerHTML = format_calendar_remaining_days;// 今年の残日数
+document.getElementById("fy-days").innerHTML = format_fiscal_remaining_days;// 今年度の残日数
